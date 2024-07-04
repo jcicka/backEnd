6d27e4486b151b9223cd282af18504209c6ef891
LordChecoxmander <101839314+LordChecoxmander@users.noreply.github.com>
2024-07-04 18:13:14 -0300

    auth controller conectado a la BD

--- controllers/authControlles.js
+++ controllers/authControlles.js
@@ -2,34 +2,46 @@ const jwt =  require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 const users = require('../models/userModels');
 const config = require('../config/config');
+const bd = require('../db/db')
 
 exports.register = (req, res) => {
-    const {username, password} = req.body
+    try{
+        const {username, password} = req.body
 
-    const hashedPassword = bcrypt.hashSync(password, 8)
+        const hashedPassword = bcrypt.hashSync(password, 8)
 
-    const newUser = {id: users.length + 1, username, password: hashedPassword}
+        bd.query('INSERT INTO users (nombre, password) VALUES (?, ?)', [username, hashedPassword])
 
-    users.push(newUser)
+        
+        const token = jwt.sign({id: results.insertId}, config.secretKey, {expiresIn: config.tokenExpiresIn} )
 
-    const token = jwt.sign({id: newUser.id}, config.secretKey, {expiresIn: config.tokenExpiresIn} )
-
-    res.status(201).send({auth: true, token})
+        res.status(201).send({auth: true, token})
+    }catch(error){
+        res.status(500).send('error en el servidor')
+    }
 }
 
 exports.login = (req, res) => {
     const {username, password} = req.body;
 
-    const user = users.find(u => u.username === username);
+    //const user = users.find(u => u.username === username);
 
-    if(!user) return res.status(404).send(`User not found`);
+    bd.query('SELECT * FROM users WHERE nombre = ?', username, (err, res)=> {
+        if (err) {
+            return res.status(500).send('error en el servidor');
+          }
+        if (results.length === 0) {
+        return res.status(404).send('usuario no encontrado');
+        }
 
-    const passwordIsValid = bcrypt.compareSync(password, user.password);
+        const user = res[0];
+        const isPassValid = bcrypt.compareSync(password, user.password)
 
-    if(!passwordIsValid) return res.status(401).send({ auth: false, token: null });
+        if(!isPassValid) return res.status(401).send({auth:false, token: null})
 
-    const token = jwt.sign({id: user.id}, config.secretKey, {expiresIn: config.tokenExpiresIn} );
-
-    res.status(200).send({auth: true, token});
+        const token = jwt.sign({id:user.id}, config.secretKey, {expiresIn: config.tokenExpiresIn})
+    
+        res.status(200).send({auth: true, token})
+    })
 
 }
